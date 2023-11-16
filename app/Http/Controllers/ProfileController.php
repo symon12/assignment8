47c288475ab61a3c1c7e49a2d0f044131800bc36
Saymon <mdsymon703@gmail.com>
2023-11-17 00:20:13 +0600

    init

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,12 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class ProfileController extends Controller
 {
-    function get(){
-        return "my name is symon";
+   public function index(Request $request){
+       $id=$request->id;
+        $name="Donal Trump";
+        $age="75";
+
+        $data=array(
+            "id"=>"$id",
+            "name"=>"$name",
+            "age"=>"$age"
+        );
+
+    $name="access_token";
+    $value="23-XYZ";
+    $minutes =1;
+    $path="/";
+    $domain=$_SERVER["SERVER_NAME"];
+    $secure=false;
+    $httpOnly=true;
+
+    $response=response()->json($data,200)->cookie(
+        $name ,$value , $minutes, $path ,$domain ,$secure , $httpOnly
+    );
+    return $response;
     }
+   
+
+   
 }
+
