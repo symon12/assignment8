bc9a9910e6aef81b3d61e9cb46a764f25144618b
Saymon <mdsymon703@gmail.com>
2023-11-17 00:20:13 +0600

    init

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/profile/{id}",[ProfileController::class,"index"]);
+
+
+
+
